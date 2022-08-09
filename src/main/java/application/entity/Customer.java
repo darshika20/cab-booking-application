a94710da3765c75f77d63fb28e9504b4604cb32f
Darshika <darshikajha05@gmail.com>
2022-08-09 12:54:59 +0530

    first commit

---
+++ src/main/java/application/entity/Customer.java
@@ -0,0 +1,16 @@
+package application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String firstName;
+}
